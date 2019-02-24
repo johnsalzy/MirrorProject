4fae53d25e89dd20e35d75a884823b60e8141bab
johnsalzy <salzyjohn@gmail.com>
2019-02-24 17:58:47 -0500

    added module

--- src/Modules/Weather.js
+++ src/Modules/Weather.js
@@ -3,9 +3,13 @@ import './Weather.css';
 
 class Weather extends Component {
   render() {
+      
+
     return (
       <div className="Weather">
         <p>This is the weather section</p>
+
+      
       </div>
     );
   }
