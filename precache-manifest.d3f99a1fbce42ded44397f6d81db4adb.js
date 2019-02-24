4f73a5c143988837bfd6a951f6ed5590f2ff515e
johnsalzy <salzyjohn@gmail.com>
2019-02-24 15:49:08 -0500

    Updates

---
+++ precache-manifest.d3f99a1fbce42ded44397f6d81db4adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38f6a4ffbf62e83c597",
+    "url": "/MirrorProject/static/css/main.aadcb3c1.chunk.css"
+  },
+  {
+    "revision": "c38f6a4ffbf62e83c597",
+    "url": "/MirrorProject/static/js/main.c38f6a4f.chunk.js"
+  },
+  {
+    "revision": "94ffe692e300b16e474a",
+    "url": "/MirrorProject/static/js/runtime~main.94ffe692.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/MirrorProject/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MirrorProject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4c65fef3d3a2e5c467b95f2d8f78f814",
+    "url": "/MirrorProject/index.html"
+  }
+];
